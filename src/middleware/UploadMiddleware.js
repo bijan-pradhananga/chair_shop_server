ba7c3aa0411dbedd26b88db6598ab43218a6e5c4
Bijan <bijan2059@gmail.com>
2024-12-17 20:48:17 +0545

    update upload code

--- src/middleware/UploadMiddleware.js
+++ src/middleware/UploadMiddleware.js
@@ -1,10 +1,25 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 // Set storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/products'); // Path to store images
+    const uploadPath = './public/products';
+    
+    // Check if the directory exists, and create it if not
+    fs.exists(uploadPath, (exists) => {
+      if (!exists) {
+        fs.mkdir(uploadPath, { recursive: true }, (err) => {
+          if (err) {
+            return cb(err, null);
+          }
+          cb(null, uploadPath);
+        });
+      } else {
+        cb(null, uploadPath); // Directory exists
+      }
+    });
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
