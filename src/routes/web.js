7e14a0b864c2b396a5fa5d39c8609ba5dad157b6
Bijan <bijan2059@gmail.com>
2024-12-21 13:13:27 +0545

    add cart

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const brandRouter = require('./BrandRouter');
+const cartRouter = require('./CartRouter');
 const categoryRouter = require('./CategoryRouter');
 const productRouter = require('./ProductRouter');
 const router = express.Router();
 
-router.use('/brand',brandRouter);
-router.use('/category',categoryRouter);
-// router.use('/order',orderRouter)
-// router.use('/brand',brandRouter)
-router.use('/product',productRouter)
-// router.use('/cart',cartRouter)
+// Other routes
+router.use('/brand', brandRouter);
+router.use('/category', categoryRouter);
+router.use('/cart', cartRouter);
+// router.use('/order', orderRouter);
+router.use('/product', productRouter);
 
-module.exports = router
+
+module.exports = router;
