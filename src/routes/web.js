68549ee48921c5887696e1ddaf65540d1adfe052
Bijan <bijan2059@gmail.com>
2024-12-23 19:18:35 +0545

    add order route

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,6 +2,7 @@ const express = require('express');
 const brandRouter = require('./BrandRouter');
 const cartRouter = require('./CartRouter');
 const categoryRouter = require('./CategoryRouter');
+const orderRouter = require('./OrderRouter');
 const productRouter = require('./ProductRouter');
 const router = express.Router();
 
@@ -9,7 +10,7 @@ const router = express.Router();
 router.use('/brand', brandRouter);
 router.use('/category', categoryRouter);
 router.use('/cart', cartRouter);
-// router.use('/order', orderRouter);
+router.use('/order', orderRouter);
 router.use('/product', productRouter);
 
 
