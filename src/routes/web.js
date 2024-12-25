cc867588382d7b51035fa79d28c829243c9b0cd1
Bijan <bijan2059@gmail.com>
2024-12-25 17:35:06 +0545

    update route

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,7 @@ const cartRouter = require('./CartRouter');
 const categoryRouter = require('./CategoryRouter');
 const orderRouter = require('./OrderRouter');
 const productRouter = require('./ProductRouter');
+const paymentRouter = require('./PaymentRouter');
 const router = express.Router();
 
 // Other routes
@@ -11,6 +12,7 @@ router.use('/brand', brandRouter);
 router.use('/category', categoryRouter);
 router.use('/cart', cartRouter);
 router.use('/order', orderRouter);
+router.use('/payment', paymentRouter);
 router.use('/product', productRouter);
 
 
